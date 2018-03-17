375cbdc7ab79264f4e52197293c8860fd10244d8
Christopher Fonseka <chris.fonseka@googlemail.com>
2018-03-17 23:25:28 +0000

    Added all locations

--- FantasyGPSimulator/Model/Location.swift
+++ FantasyGPSimulator/Model/Location.swift
@@ -11,4 +11,24 @@ import Foundation
 enum Location: String
 {
     case Australia
+    case Bahrain
+    case China
+    case Azerbaijan
+    case Spain
+    case Monaco
+    case Canada
+    case France
+    case Austria
+    case Britain
+    case Germany
+    case Hungary
+    case Belgium
+    case Italy
+    case Singapore
+    case Russia
+    case Japan
+    case USA
+    case Mexico
+    case Brazil
+    case AbuDhabi
 }
