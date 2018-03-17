4936f40c79b1ca9b656efe4d19a7e695e9fecdcf
Christopher Fonseka <chris.fonseka@googlemail.com>
2018-03-17 19:50:12 +0000

    Initial Commit

---
+++ FantasyGPSimulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FantasyGPSimulator
+//
+//  Created by Christopher Fonseka on 17/03/2018.
+//  Copyright © 2018 Christopher Fonseka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
