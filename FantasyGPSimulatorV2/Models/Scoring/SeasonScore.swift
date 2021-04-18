6a428610f6a753ab1568e2f216cd784d406f5c65
Christopher Fonseka <chris.fonseka@googlemail.com>
2021-04-18 19:13:46 +0100

    Added SeasonScore to get a full season overview

---
+++ FantasyGPSimulatorV2/Models/Scoring/SeasonScore.swift
@@ -0,0 +1,26 @@
+//
+//  SeasonScore.swift
+//  FantasyGPSimulatorV2
+//
+//  Created by Christopher Fonseka on 18/04/2021.
+//
+
+import Foundation
+
+struct SeasonScore: Score {
+    let scoreArray: [Score] = {
+        Races.all.map(RaceScore.init)
+    }()
+
+    func score(for driver: Driver) -> Int {
+        scoreArray
+            .map { $0.score(for: driver) }
+            .reduce(0, +)
+    }
+
+    func score(for constructor: Constructor) -> Int {
+        scoreArray
+            .map { $0.score(for: constructor) }
+            .reduce(0, +)
+    }
+}
